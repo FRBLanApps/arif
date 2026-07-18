5e9ced2a182683b434f3d751097e68c4f097dd63
furrybluelan <furrybluelan@disroot.org>
2026-07-18 20:57:34 +0800

    Rename application package id to org.frblanapps.arif
    Update Android applicationId/namespace, iOS bundle id, Linux APPLICATION_ID, and Windows company metadata.

--- apps/arif/android/app/build.gradle.kts
+++ apps/arif/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.arif.arif"
+    namespace = "org.frblanapps.arif"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.arif.arif"
+        applicationId = "org.frblanapps.arif"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- apps/arif/android/app/src/main/kotlin/org/frblanapps/arif/MainActivity.kt
+++ apps/arif/android/app/src/main/kotlin/org/frblanapps/arif/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.arif.arif
+package org.frblanapps.arif
 
 import io.flutter.embedding.android.FlutterActivity
 
